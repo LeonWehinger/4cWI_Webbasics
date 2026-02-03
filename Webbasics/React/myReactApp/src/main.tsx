901b1e9a642d519ed94cab136a8b34664b033bec
Leon <leon.wehinger@htldornbirn.at>
2026-02-03 10:27:48 +0100

    A9 Router finished

--- Webbasics/React/myReactApp/src/main.tsx
+++ Webbasics/React/myReactApp/src/main.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import A5_2_App from "./A5.2_App.tsx";
-import A8_App from "./A8_App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./index.css";
+
+import A2_App from "./A2_App";
+import A5_1_App from "./A5.1_App";
+import A5_2_App from "./A5.2_App";
+import A7_1_App from "./A7.1_App";
+import A7_2_App from "./A7.2_App";
+import A8_App from "./A8_App";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <A5_2_App/>,
-    },
-    {
-        path: "/A8",
-        element: <A8_App/>,
-    },
+    { path: "/", element: <A2_App /> },
+    { path: "/A5.1", element: <A5_1_App /> },
+    { path: "/A5.2", element: <A5_2_App /> },
+    { path: "/A7.1", element: <A7_1_App /> },
+    { path: "/A7.2", element: <A7_2_App /> },
+    { path: "/A8", element: <A8_App /> },
 ]);
 
-const root = document.getElementById("root");
+const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(
-    <RouterProvider router={router} />,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
-
